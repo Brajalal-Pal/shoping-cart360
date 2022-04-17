2d1567bdcd332da69805310604dffec6bc452806
Brajalal Pal <brajalal.pal@gmail.com>
2022-04-17 21:53:43 +0530

    Author image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import AuthorImage from "./images/author.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my React Application</h1>
+      <img src={AuthorImage} height="300" alt="" />
+      <h2>Author: Brajalal Pal</h2>
     </div>
   );
 }
